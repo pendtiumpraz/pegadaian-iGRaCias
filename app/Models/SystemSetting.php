87c650ab97655e35d22f886626e2dab4886e6998
Pegadean Dev <development@privasimu.com>
2026-05-09 16:25:00 +0700

    db: model SystemSetting

---
+++ app/Models/SystemSetting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SystemSetting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+        'description',
+    ];
+
+    protected function casts(): array
+    {
+        return [];
+    }
+}
